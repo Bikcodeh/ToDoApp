f5f50684561e5513ca0e0539988471e13a449060
Bikcodeh <bikcodeh@gmail.com>
2022-07-07 23:17:48 -0500

    design add note

--- app/src/main/java/com/bikcodeh/todoapp/MainActivity.kt
+++ app/src/main/java/com/bikcodeh/todoapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.bikcodeh.todoapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.bikcodeh.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -15,5 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/bikcodeh/todoapp/ui/fragments/notes/NotesFragment.kt
+++ app/src/main/java/com/bikcodeh/todoapp/ui/fragments/notes/NotesFragment.kt
@@ -1,19 +1,42 @@
 package com.bikcodeh.todoapp.ui.fragments.notes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bikcodeh.todoapp.R
+import com.bikcodeh.todoapp.databinding.FragmentNotesBinding
 
 class NotesFragment : Fragment() {
+
+    private var _binding: FragmentNotesBinding? = null
+    private val binding: FragmentNotesBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_notes, container, false)
+    ): View {
+        _binding = FragmentNotesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setUpListeners() {
+        binding.addNoteFab.setOnClickListener {
+            findNavController().navigate(R.id.action_notesFragment_to_addFragment)
+        }
     }
 
 }
