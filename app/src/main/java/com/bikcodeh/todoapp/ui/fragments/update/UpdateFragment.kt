200b4ba8d44d162d9a6bc290d0629da7ec169e5a
Bikcodeh <bikcodeh@gmail.com>
2022-07-08 21:12:38 -0500

    crate menu for update fragment

--- app/src/main/java/com/bikcodeh/todoapp/ui/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/bikcodeh/todoapp/ui/fragments/update/UpdateFragment.kt
@@ -1,10 +1,11 @@
 package com.bikcodeh.todoapp.ui.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import com.bikcodeh.todoapp.R
 
 class UpdateFragment : Fragment() {
@@ -14,6 +15,21 @@ class UpdateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.update_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
